1f69de36509ebe7045de834a06eccf6a4bf524a1
samarth <info@scubecs.com>
2019-10-19 23:50:20 -0700

    Updayed the flutter version

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/samarth/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/samarth/Documents/flutter-things-todo"
+export "FLUTTER_TARGET=/Users/samarth/Documents/flutter-things-todo/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/samarth/Documents/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
